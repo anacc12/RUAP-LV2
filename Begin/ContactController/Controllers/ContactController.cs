74d3b937356336fb8701399fb24cf09e87fbb6c4
Ana <cacijaana2@gmail.com>
2019-11-07 18:22:31 +0100

    Task 3

--- Begin/ContactController/Controllers/ContactController.cs
+++ Begin/ContactController/Controllers/ContactController.cs
@@ -4,18 +4,26 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ContactController.Models;
 
 namespace ContactController.Controllers
 {
     public class ContactController : ApiController
     {
-        public string[] Get()
+        public Contact[] Get()
         {
-            return new string[]
-    {
-        "Hello",
-        "World"
-    };
+            return new Contact[]{
+                new Contact{
+                    Id = 1,
+                    Name = "Glenn Block"
+                    },
+
+                new Contact{
+                     Id = 2,
+                     Name = "Dan Roth"
+                     }
+            };
+    
         }
     }
 }
